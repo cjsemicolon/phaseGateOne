2865686f42a8963ffc2107d62591772a610feda7
cjsemicolon <cjsemicolon@gmail.com>
2026-05-20 12:25:16 +0100

    Phase Gate day Three morning

---
+++ dayThree/morningSession/java/faceBookLikeSystemFunction.java
@@ -0,0 +1,49 @@
+//write a function that takes an array as input
+//initialize count as zero
+//loop through the array to find out if it has content
+//if the array has content count + 1
+//if not print no one likes this
+//if it has content print index likes this
+//if content = 2 print index "and" index like this
+//if content = 3 print index "," index ", and" index like this
+//if content = 4 print index1 index2 and "2 others like this"
+//NA TO CONVERT PSUEDO CODE TO JAVA NAI REMAIN SO O!!!
+
+
+public class faceBookLikeSystemFunction {
+
+    public static String checkIfPeopleLiked (String[] names) {
+
+        int count = 0;
+
+        for (String character : names){
+
+            count ++; 
+
+         if(count == 0) {
+        
+            return "no one likes this";
+
+        }
+
+//    
+//        return count;
+
+    
+
+//    public static String displayLikeMessage (String[] names) {
+//
+//       
+
+//        }else if(count == 1){
+//
+//            return [0] "likes this";
+//
+//        }
+
+}    
+}
+
+
+
+}
---
+++ dayThree/morningSession/java/faceBookLikeSystemFunctionTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class faceBookLikeSystemFunctionTest {
+
+    @Test
+    public void testThatTheArrayReturnsACount() {
+
+        String[] input = {};
+
+        String expected = "no one likes this";
+
+        String actual = faceBookLikeSystemFunction.checkIfPeopleLiked(input);
+
+        assertArrayEquals(expected, actual);
+
+    }
+
+}
